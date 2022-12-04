9172b7378facc10d5ee0fb962904bf66057126b8
tranchikhang <tranchikhang@outlook.com>
2022-12-04 22:10:07 +0900

    day 4

---
+++ 2022/day-4/solution2.php
@@ -0,0 +1,26 @@
+<?php
+
+$total = 0;
+
+if ($file = fopen("input.txt", "r")) {
+    while(!feof($file)) {
+        $line = trim(fgets($file));
+        $arr = explode(',', $line);
+
+        $first = explode("-", $arr[0]);
+        $second = explode("-", $arr[1]);
+
+        if (($first[0] <= $second[0] && $first[1] >= $second[1]) ||
+            $first[0] >= $second[0] && $first[1] <= $second[1]) {
+            $total +=1;
+        } elseif ($first[1]>=$second[1] && $first[0]<=$second[1]) {
+            $total +=1;
+        } elseif ($second[1]>=$first[1] && $second[0]<=$first[1]) {
+            $total +=1;
+        }
+
+
+     }
+    fclose($file);
+}
+echo $total;
